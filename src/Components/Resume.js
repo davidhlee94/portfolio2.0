f16d525ef38843f6d8aa00674dc2dac6c267bf9e
davidhlee94 <davidhlee94@gmail.com>
2023-02-09 14:46:28 -0800

    iframe

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,16 +8,6 @@ const Resume = () => {
             <div className="resume-title-container">
                 <h1 className="resume">RESUME</h1>
             </div>
-            <div className="resume-instruction-container">
-                <h3 className="resume-instruction">
-                    View my resume below or click button below to download!
-                </h3>
-            </div>
-            <div className="download-row">
-                <a href="DavidLeeSWEResume.pdf" download="DavidLeeSWEResume.pdf">
-                    <button className="download-button">DOWNLOAD</button>
-                </a>
-            </div>
             <iframe
                 src="https://drive.google.com/file/d/1qBCi78O1gKp9OGvohT0tEAPH4kHbGQPi/preview"
                 allow="autoplay"
